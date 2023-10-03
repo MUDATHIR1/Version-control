12e8376625f0cc9e6a56646898f1fe187d7f7d75
MUDATHIR1 <salaudeenmudathir1@gmail.com>
2023-10-03 11:25:56 +0300

    Added the values of the ints created

--- My First App/src/MyFirstApp.java
+++ My First App/src/MyFirstApp.java
@@ -20,6 +20,21 @@ System.out.println("This app is made by:");
 
 System.out.println("This app is made by " + creator);
 
+System.out.println("num1");
+System.out.println("The value of num1 is " +  num1);
+System.out.println("The value of num2 is " +  num2);
+
+
+System.out.println("The value of num1 * num2 is " + multiplication );
+System.out.println(num1 + " * num2 = 128");
+
+
+System.out.println("The value of num1 is " +  num1);
+System.out.println("The value of num2 is " +  num2);
+
+System.out.println(num1 / num2);
+System.out.println("The value of num1 / num2 is " + division);
+
 
 
 
