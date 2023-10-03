1bd6ba550c26b564c4af029e4565db20ed68dcf6
MUDATHIR1 <salaudeenmudathir1@gmail.com>
2023-10-03 11:06:06 +0300

    This App was made by me printed to the console

---
+++ My First App/src/MyFirstApp.java
@@ -0,0 +1,31 @@
+
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am creator");
+
+
+String creator = "Mudathir";
+int num1 = 16;
+int num2 = 8;
+int multiplication = num1 * num2;
+double division = num1 / num2;
+
+
+System.out.println("......Printer App......");
+
+System.out.println("This app is made by:");
+
+
+
+System.out.println("This app is made by " + creator);
+
+
+
+
+
+
+	}
+
+}
